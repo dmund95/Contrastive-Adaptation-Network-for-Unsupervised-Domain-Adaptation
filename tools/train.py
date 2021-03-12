78c8e92aec0914f5654e042a114a143ec983b26d
divyanshumund <divimund95@gmail.com>
2021-03-12 09:32:53 -0800

    can-fix

--- tools/train.py
+++ tools/train.py
@@ -107,7 +107,13 @@ def train(args):
        dpn.cuda()
 
     # initialize solver
-    train_solver = Solver(net, dpn, dataloaders, bn_domain_map=bn_domain_map, resume=resume_dict)
+    if args.method == 'CAN': 
+        train_solver = Solver(net, dataloaders, bn_domain_map=bn_domain_map, resume=resume_dict)
+    elif args.method == 'soft_MMD':
+        train_solver = Solver(net, dpn, dataloaders, bn_domain_map=bn_domain_map, resume=resume_dict)
+    else:
+        raise NotImplementedError("Currently don't support the specified method: %s." 
+                                 % args.method)
 
     # train 
     train_solver.solve()
